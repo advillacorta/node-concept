b40e657c8670e6958a6040652f2a294d1169376e
Andres Deza Villacorta <adeza.villacorta@gmail.com>
2016-04-12 21:39:50 +0000

    Se avanzo con controller de cargos

--- concepttest-app/public/scripts/controllers/app.ui.admin.position.js
+++ concepttest-app/public/scripts/controllers/app.ui.admin.position.js
@@ -3,75 +3,150 @@
 
 angular.module("app.ui.admin.position", [])
 
-// List Controller 
-.controller("ListPositions", ["$scope", "$filter", function($scope, $filter) {
-	// data
-	$scope.datas = [
-		{name: "Analista funcional", description: "Encargado del levantamiento y analisis de la informacion."},
-		{name: "Analista tecnico", description: "Encargado de traducir los requerimientos funcionales a componentes tecnicos."},
-		{name: "Analista programador", description: "Encargado del desarrollo de aplicaciones."},
-	];
-	var prelength = $scope.datas.length;
-
-	// create random data (uses `track by $index` in html for duplicacy)
-	/*
-	for(var i = prelength; i < 100; i++) {
-		var rand = Math.floor(Math.random()*prelength);
-		$scope.datas.push($scope.datas[rand]);
-	}
-	*/
+// Listado de posiciones
+.controller("ListPositions", ["$route", "$scope", "$filter", "$window", "$location", "positionService", 
+	function($route, $scope, $filter, $window, $location, positionService) 
+	{
+		$scope.datas;
+		$scope.searchKeywords;
+		$scope.filteredData;
+		$scope.numPerPageOpts;
+		$scope.numPerPage;
+		$scope.currentPage;
+		$scope.currentPageStores;
+		
+		function getPositions()
+		{
+			positionService.getPositions()
+			.then(function(response)
+			{
+				$scope.datas = response.data;
+				
+				$scope.searchKeywords = "";
+				$scope.filteredData = [];	
+				$scope.row = "";
 	
-	$scope.searchKeywords = "";
-	$scope.filteredData = [];	
-	$scope.row = "";
+				$scope.numPerPageOpts = [5, 7, 10, 25, 50, 100];
+				$scope.numPerPage = $scope.numPerPageOpts[1];
+				$scope.currentPage = 1;
+				$scope.currentPageStores = []; // data to hold per pagination
+	
+				$scope.select = function(page) 
+				{
+					var start = (page - 1)*$scope.numPerPage,
+						end = start + $scope.numPerPage;
+	
+					$scope.currentPageStores = $scope.filteredData.slice(start, end);
+				}
+	
+				$scope.onFilterChange = function() 
+				{
+					$scope.select(1);
+					$scope.currentPage = 1;
+					$scope.row = '';
+				}
+	
+				$scope.onNumPerPageChange = function() 
+				{
+					$scope.select(1);
+					$scope.currentPage = 1;
+				}
+	
+				$scope.onOrderChange = function() 
+				{
+					$scope.select(1);
+					$scope.currentPage = 1;
+				}
+	
+				$scope.search = function() 
+				{
+					$scope.filteredData = $filter("filter")($scope.datas, $scope.searchKeywords);
+					$scope.onFilterChange();
+				}
+	
+				$scope.order = function(rowName) 
+				{
+					if($scope.row == rowName)
+						return;
+					$scope.row = rowName;
+					$scope.filteredData = $filter('orderBy')($scope.datas, rowName);
+					$scope.onOrderChange();
+				}
+	
+				// init
+				$scope.search();
+				$scope.select($scope.currentPage);
+			});
+		}
+		
+		$scope.edit = function(id)
+		{
+			$window.location.href = '/#/admin/editPosition?id=' + id;
+		}
 
-
-	$scope.numPerPageOpts = [5, 7, 10, 25, 50, 100];
-	$scope.numPerPage = $scope.numPerPageOpts[1];
-	$scope.currentPage = 1;
-	$scope.currentPageStores = []; // data to hold per pagination
-
-
-	$scope.select = function(page) {
-		var start = (page - 1)*$scope.numPerPage,
-			end = start + $scope.numPerPage;
-
-		$scope.currentPageStores = $scope.filteredData.slice(start, end);
+		$scope.delete = function(id)
+		{
+			if(confirm("¿Está seguro que desea eliminar el cargo seleccionado?"))
+			{
+				positionService.deletePosition(id)
+				.then(function(response)
+				{
+					$route.reload();
+				});
+			}
+		}
 	}
+])
 
-	$scope.onFilterChange = function() {
-		$scope.select(1);
-		$scope.currentPage = 1;
-		$scope.row = '';
+// Registro de cargo
+.controller('NewPosition', ["$scope", "$window", "positionService", "skillService", "skillLevelService", 
+	function($scope, $window, positionService, skillService, skillTypeService)
+	{
+		$scope.init = function()
+		{
+			
+		}
+		
+		$scope.save = function(isValid)
+		{
+			if(isValid)
+			{
+				var data = $scope.skill;
+				positionService.createPosition(data)
+				.then(function(response)
+				{
+					alert("Registro satisfactorio");
+					$window.location.href = '/#/admin/positions';
+				});
+			}
+		}
+		
+		$scope.cancel = function()
+		{
+			$window.location.href = '/#/admin/positions';
+		}
 	}
+])
 
-	$scope.onNumPerPageChange = function() {
-		$scope.select(1);
-		$scope.currentPage = 1;
+// Edicion de cargo
+.controller('NewPosition', ["$scope", "$window", "positionService", "skillService", "skillLevelService", 
+	function($scope, $window, positionService, skillService, skillTypeService)
+	{
+		$scope.init = function()
+		{
+			
+		}
+		
+		$scope.save = function(isValid)
+		{
+			
+		}
+		
+		$scope.cancel = function()
+		{
+			$window.location.href = '/#/admin/positions';
+		}
 	}
-
-	$scope.onOrderChange = function() {
-		$scope.select(1);
-		$scope.currentPage = 1;
-	}
-
-
-	$scope.search = function() {
-		$scope.filteredData = $filter("filter")($scope.datas, $scope.searchKeywords);
-		$scope.onFilterChange();
-	}
-
-	$scope.order = function(rowName) {
-		if($scope.row == rowName)
-			return;
-		$scope.row = rowName;
-		$scope.filteredData = $filter('orderBy')($scope.datas, rowName);
-		$scope.onOrderChange();
-	}
-
-	// init
-	$scope.search();
-	$scope.select($scope.currentPage);
-}])
+])	
 
 }())
