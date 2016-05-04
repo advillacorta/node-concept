7d142c42a88d953a701651c568d0b0636bda5a4a
Andres Deza Villacorta <adeza.villacorta@gmail.com>
2016-05-04 14:53:15 +0000

    Se agrego configuracion de grunt para compilar archivos .less a .css

---
+++ concepttest-app/Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "public/styles/main.css": "public/styles/main.less" // destination file and source file
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['public/styles/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
+};
